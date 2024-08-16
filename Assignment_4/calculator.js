bc838d5d39159d1729c4b577f02634b109497606
Shakeel <shakeelali12pk@gmail.com>
2024-08-16 18:58:13 +0500

    Assignment 4 Completed ..

--- Assignment_4/calculator.js
+++ Assignment_4/calculator.js
@@ -26,6 +26,7 @@ function calculator(num1,num2){
     if(num2 ==0){
         console.log("Division by 0 is not allowed.")
     }
+
     else{
         let division = num1 / num2
         result.push(division)
@@ -36,21 +37,27 @@ function calculator(num1,num2){
 rl.question("Enter number 1:",(num1)=>{
 
    rl.question("Enter number 2:",(num2) =>{
-    num1 = parseInt(num1)
+    num1 = parseInt(num1) // converting String into number
 
     num2 = parseInt(num2)
    
-    calculator(num1,num2)
-
-    for(let value of result){
+    calculator(num1,num2) // Running function on user input
+    
+    let sum =0
+   
+    for(let value of result){ //filtering arrays value 
 
         console.log(value)
-
+        sum+=value
+    
     }
+    let avg = sum / result.length  //Finding average of all values 
+    
+    console.log(`Average :${avg} `)
+
     rl.close()
    })
 })
-
 
 
 
