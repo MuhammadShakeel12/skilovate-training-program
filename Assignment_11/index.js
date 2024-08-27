e510bb8dd634f5993b51844078567eecdb2a05d1
Shakeel <shakeelali12pk@gmail.com>
2024-08-27 15:47:40 +0500

    Assignment 11

---
+++ Assignment_11/index.js
@@ -0,0 +1,10 @@
+// Chalk is a third-party module of node.js that is used for styling of output
+// like : we make the warnings as red coloured, to increase the clarity.
+
+// Example 
+
+import chalk from 'chalk'
+
+console.log(chalk.white.underline.bgBlue('Welcome to skilovate'))
+console.log(chalk.black.bgYellow.italic('Your MERN Stack Journey Begins Here'))
+console.log(chalk.magenta("Get ready to code and build amazing projects"))
